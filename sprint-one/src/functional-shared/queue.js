abe8b9de0cce75dcd04352136ee0e3398b9f9396
Harish Kilaru <hckilaru@gmail.com>
2016-06-02 13:41:11 -0400

    fixed functional shared queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -2,6 +2,7 @@ var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var obj = {};
+  obj.storage = {};
   _.extend(obj, queueMethods);
   return obj;
 
@@ -9,26 +10,20 @@ var Queue = function(){
 var queueMethods = {
   count: 0,
   enqueue: function(value){
-    var keyArray = Object.keys(this);
-    console.log(keyArray);
-    for(var i = 0; i < Object.keys(this).length; i++) {
-      this[Object.keys(this)[i + 1]] = this[Object.keys(this)[i]];
-    }
-    console.log(keyArray);
-    Object.keys(this).unshift("newValue");
-    this["newValue"] = value;
+      for(var i = 0; i < Object.keys(this.storage).length; i++) {
+       this.storage[Object.keys(this.storage)[i + 1]] = this.storage[Object.keys(this.storage)[i]];
+     }
+     this.storage[Object.keys(this.storage)[0]] = value;
     this.count++;
-
   },
   dequeue: function(){
-    var dequeuedElement = this[Object.keys(this)[Object.keys(this).length - 1]];
-    delete this[Object.keys(this)[Object.keys(this).length - 1]];
+    var dequeuedElement = this.storage[Object.keys(this.storage)[Object.keys(this.storage).length - 1]];
+    delete this.storage[Object.keys(this.storage)[Object.keys(this.storage).length - 1]];
     if(this.count > 0) {
-    this.count--;
+     this.count--;
     }
-    return dequeuedElement;
+     return dequeuedElement;
   },
-
   size: function(){
     return this.count;
   }
