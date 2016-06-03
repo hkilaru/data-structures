6f0dbde4996c2f2a52b547385b6c42f1c16ab775
MKS-students <mks.students@hackreactor.com>
2016-06-03 15:59:59 -0400

    finished tree and most of graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,21 +1,59 @@
 
 
 var Graph = function(){
+  this.nodes = [];
 };
 
 Graph.prototype.addNode = function(node){
+  var newNode = {};
+  newNode.edges = [];
+  newNode.name = node;
+  this.nodes.push(newNode);
+
 };
 
 Graph.prototype.contains = function(node){
+
+  var found = false ;
+
+  for(var i = 0; i < this.nodes.length; i++) {
+    if(this.nodes[i].name === node) {
+      found = true;
+      return found;
+    }
+  }
+  return found;
 };
 
 Graph.prototype.removeNode = function(node){
+  for(var i = 0; i < this.nodes.length; i++) {
+    if(this.nodes[i].name === node) {
+      var spliced = this.nodes.splice(i, i + 1);
+    }
+  }
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  var hasEdge = false;
+  for(var i = 0; i < this.nodes.length; i++) {
+    if(this.nodes[i].name === fromNode) {
+      for(var j = 0; j < this.nodes[i].edges.length; j++) {
+        if(this.nodes[i].edges[j] === toNode) {
+          hasEdge = true;
+        }
+      }
+    }
+  }
+  return hasEdge;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  for(var i = 0; i < this.nodes.length; i++) {
+    if(this.nodes[i].name === fromNode) {
+      this.nodes[i].edges.push(toNode);
+    }
+  }
+
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
@@ -27,6 +65,3 @@ Graph.prototype.forEachNode = function(cb){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
-
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,11 +1,11 @@
 var Tree = function(value){
-  debugger;
+
   var newTree = {};
   newTree.value = value;
 
   // your code here
   _.extend(newTree, treeMethods);
-  newTree.children = newTree.childNodes;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -18,12 +18,33 @@ var treeMethods = {};
 
 treeMethods.addChild = function(value){
 
+  var child = Tree(value);
+  child.value = value;
+  this.children.push(child);
+
   // this.children.push(value);
   // console.log(this.children);
 
 };
 
 treeMethods.contains = function(target){
+  var contains = false;
+  var findValue = function(node) {
+    if(node.value && node.value === target) {
+      contains = true;
+      return contains;
+    }
+    else {
+      if(node.children) {
+        for(var i = 0; i < node.children.length; i++) {
+          findValue(node.children[i]);
+        }
+      }
+    }
+    return contains;
+  };
+  var find = findValue(this);
+  return find;
 
 };
 
