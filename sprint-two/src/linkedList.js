f698f0842a11f2da3819ae46add3b3252fd63d3d
MKS-students <mks.students@hackreactor.com>
2016-06-02 20:29:46 -0400

    finished removeHead and addTail

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,9 +4,34 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+
+    //need to save current tail
+    //need to point list.tail to new tail
+    //need to point previous tail to new tail
+    var newTail = Node(value);
+    if(list.tail && list.tail.value !== null) {
+      var currentTail = list.tail;
+      currentTail.next = newTail;
+    }
+    list.tail = newTail;
+    if(list.head === null) {
+      list.head = newTail;
+    }
+
+    // this.tail.next = null;
+    // console.log(this.tail);
+    // console.log("list :" + list)
+    // console.log("node :" + this.tail.next)
+
   };
 
   list.removeHead = function(){
+    debugger;
+    if(list.head !== null) {
+      var currentHead = list.head;
+      list.head = currentHead.next;
+    }
+    return currentHead.value;
   };
 
   list.contains = function(target){
@@ -27,3 +52,5 @@ var Node = function(value){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+// if list.tail === null, list.tail[];
+//
